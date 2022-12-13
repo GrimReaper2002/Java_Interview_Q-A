35704fed11d5a3f51e98310ff37d419ed33c2e4d
GrimReaper2002 <92012986+GrimReaper2002@users.noreply.github.com>
2022-12-13 12:16:39 +0530

    Add files via upload

---
+++ StackCollections.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class MyStack{
+	public static void main(String args[]){
+		//Stack<Integer> s = new Stack<Interger>;
+		ArrayDeque<Integer> s = new ArrayDeque<Integer>();
+		s.push(10);
+		s.push(20);
+		s.push(30);
+		System.out.println(s.pop());
+		System.out.println(s.peek());
+		System.out.println(s.isEmpty());
+		System.out.println(s.size());
+	}
+}
