6b1f68f3d36a8fdcbee393a295d03bffc522f2cf
GrimReaper2002 <92012986+GrimReaper2002@users.noreply.github.com>
2022-12-13 11:48:06 +0530

    Add files via upload

---
+++ StackArrayList.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class MyStack{
+	ArrayList<Integer> arr = new ArrayList<Integer>();
+	//adding element to stack
+	void push(int x){
+		arr.add(x);
+	}
+	
+	//removing top element in stack
+	int pop(){
+		int res = arr.get(arr.size()-1);
+		arr.remove(arr.size()-1);
+		return res;
+	}
+	
+	//retuns top element in stack
+	int peek(){
+		return arr.get(arr.size()-1);
+	}
+	boolean isEmpty(){
+		return arr.isEmpty();
+	}
+	int size(){
+		return arr.size();
+	}
+	public static void main(String args[]){
+		MyStack s = new MyStack();
+		s.push(10);
+		s.push(20);
+		s.push(30);
+		s.push(40);
+		System.out.println(s.pop());
+		System.out.println(s.peek());
+		System.out.println(s.isEmpty());
+		System.out.println(s.size());
+	}
+}
